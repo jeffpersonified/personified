468308828f915adcb1c3380624652982da908893
jeffpersonified <jeff.matthew.smith@gmail.com>
2013-08-17 12:47:23 -0700

    initial commit, yo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personified::Application.config.secret_token = 'e75501b390d82170884652fb939d47d8e832c6a43d4093102f1296c83c24c29c574567e7a9eb2e04e7cd3ece6271524a34564955ee4ec1d3a303b748255f756a'
